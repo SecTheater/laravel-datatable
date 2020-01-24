cff298356ba88b5990320fc7ce15d37a4f019de8
mohammed osama <mohammedosama@ieee.org>
2020-01-25 01:59:50 +0200

    add basic signature for the command to run package

--- src/Commands/RegisterServiceDataTableCommand.php
+++ src/Commands/RegisterServiceDataTableCommand.php
@@ -6,6 +6,11 @@ use Illuminate\Console\Command;
 
 class RegisterServiceDataTableCommand extends Command
 {
+    /**
+     * @var string
+     */
+    protected $signature = 'datatable:register';
+
     public function handle()
     {
 
